b1bc96beeada5a7253f724800bfc1b8182f4467e
Wargen Guittap <48695406+guittap@users.noreply.github.com>
2021-05-16 17:01:31 -0700

    api fetch

--- ran-rest/src/App.js
+++ ran-rest/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      items: [],
+
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972")
+      .then(res => res.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+  
+        })
+      });
+  }
+  render() {
+
+    const { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    }
+
+    else {
+      return (
+        <div className="App">
+          Data has been loaded!
+        </div>
+      );
+    }
+    
+    }
 }
 
 export default App;
